2d794c33571d2c056b2b6aff2886cb1edf715c9b
oktayudha05 <oktayudha05@gmail.com>
2024-09-13 00:40:02 +0700

    domisili done, start sktm

--- src/sktm/script.js
+++ src/sktm/script.js
@@ -39,7 +39,6 @@ document
 
     let name = document.getElementById("nama_lengkap").value;
     let jk = document.getElementById("jenis_kelamin").value;
-    let bin = document.getElementById("bin_binti").value;
     let agama = document.getElementById("agama").value;
     let temLahir = document.getElementById("tempat_lahir").value;
     let tangLahir = document.getElementById("tanggal_lahir").value;
@@ -47,9 +46,12 @@ document
     let nik = document.getElementById("no_ktp_nik").value;
     let pekerjaan = document.getElementById("pekerjaan").value;
     let alamat = document.getElementById("alamat").value;
+    let rt = document.getElementById("rt").value;
+    if (rt.length === 1) {
+      rt = `0${rt}`;
+    }
 
     name = toTitleCase(name);
-    bin = toTitleCase(bin);
     agama = toTitleCase(agama);
     temLahir = toTitleCase(temLahir);
     tangLahir = formatTanggal(tangLahir);
@@ -57,64 +59,91 @@ document
     wargaNegara = toTitleCase(wargaNegara);
     pekerjaan = toTitleCase(pekerjaan);
     alamat = toTitleCase(alamat);
+    const tanggal = formatTanggal(new Date());
 
     // Isi PDF
     await fillPDF(
       name,
       jk,
-      bin,
       lahir,
       agama,
       wargaNegara,
       nik,
       pekerjaan,
-      alamat
+      alamat,
+      rt,
+      tanggal
     );
   });
 
 const fillPDF = async (
   name,
   jk,
-  bin,
   lahir,
-  agama,
   wargaNegara,
   nik,
   pekerjaan,
-  alamat
+  alamat,
+  rt,
+  tanggal
 ) => {
   const { PDFDocument, rgb } = PDFLib;
 
   // Ambil template
-  const url = "document/SuratKeteranganDomisili.pdf";
+  const url = "document/sktm.pdf";
   const existingPdfBytes = await fetch(url).then((res) => res.arrayBuffer());
 
   const pdfDoc = await PDFDocument.load(existingPdfBytes);
   const pages = pdfDoc.getPages();
   const firstPage = pages[0];
-  let tinggiAwal = 278;
-  let x = 225;
+  let tinggiAwal = 347;
+  let panjangAwal = 0;
+  let x = 265;
 
   const format = () => {
     const { width, height } = firstPage.getSize();
 
     return {
-      x: x,
+      x: x + panjangAwal,
       y: height - tinggiAwal,
       size: 11,
       color: rgb(0, 0, 0),
     };
   };
 
-  firstPage.drawText(name, format(), (tinggiAwal += 22));
-  firstPage.drawText(jk, format(), (tinggiAwal += 22));
-  firstPage.drawText(bin, format(), (tinggiAwal += 22));
-  firstPage.drawText(lahir, format(), (tinggiAwal += 22));
-  firstPage.drawText(agama, format(), (tinggiAwal += 22));
-  firstPage.drawText(wargaNegara, format(), (tinggiAwal += 22));
-  firstPage.drawText(nik, format(), (tinggiAwal += 22));
-  firstPage.drawText(pekerjaan, format(), (tinggiAwal += 22));
-  firstPage.drawText(alamat, format(), (tinggiAwal += 22));
+  firstPage.drawText(name, format(), (tinggiAwal += 15));
+  firstPage.drawText(jk, format(), (tinggiAwal += 16));
+  //firstPage.drawText(bin, format(), (tinggiAwal += 22));
+  firstPage.drawText(lahir, format(), (tinggiAwal += 16));
+  firstPage.drawText(wargaNegara, format(), (tinggiAwal += 16));
+  //firstPage.drawText(agama, format(), (tinggiAwal += 22));
+  firstPage.drawText(nik, format(), (tinggiAwal += 16));
+  firstPage.drawText(pekerjaan, format(), (tinggiAwal += 16));
+  firstPage.drawText(alamat, format(), (tinggiAwal += 16));
+  firstPage.drawText(rt, {
+    x: 398,
+    y: 442,
+    size: 11,
+    color: rgb(0, 0, 0),
+  });
+  firstPage.drawText(tanggal, {
+    x: 455,
+    y: 442,
+    size: 11,
+    color: rgb(0, 0, 0),
+  });
+  firstPage.drawText(tanggal, {
+    x: 433,
+    y: 265,
+    size: 11,
+    color: rgb(0, 0, 0),
+  });
+  firstPage.drawText("Sodikun", {
+    x: 427,
+    y: 163,
+    size: 11,
+    color: rgb(0, 0, 0),
+  });
 
   const pdfBytes = await pdfDoc.save();
 
