bc894ceff7e2694cb37592e87e11e48d8ec8fc9a
oktayudha05 <oktayudha05@gmail.com>
2024-09-16 15:26:51 +0700

    fillable file from win

--- src/sktm/script.js
+++ src/sktm/script.js
@@ -39,7 +39,6 @@ document
 
     let name = document.getElementById("nama_lengkap").value;
     let jk = document.getElementById("jenis_kelamin").value;
-    let agama = document.getElementById("agama").value;
     let temLahir = document.getElementById("tempat_lahir").value;
     let tangLahir = document.getElementById("tanggal_lahir").value;
     let wargaNegara = document.getElementById("warganegara").value;
@@ -52,21 +51,19 @@ document
     }
 
     name = toTitleCase(name);
-    agama = toTitleCase(agama);
     temLahir = toTitleCase(temLahir);
     tangLahir = formatTanggal(tangLahir);
     const lahir = `${temLahir}, ${tangLahir}`;
     wargaNegara = toTitleCase(wargaNegara);
     pekerjaan = toTitleCase(pekerjaan);
     alamat = toTitleCase(alamat);
-    const tanggal = formatTanggal(new Date());
+    const tanggal = `${formatTanggal(new Date())}`;
 
     // Isi PDF
     await fillPDF(
       name,
       jk,
       lahir,
-      agama,
       wargaNegara,
       nik,
       pekerjaan,
@@ -90,60 +87,21 @@ const fillPDF = async (
   const { PDFDocument, rgb } = PDFLib;
 
   // Ambil template
-  const url = "document/sktm.pdf";
+  const url = "document/sktm-fil1.pdf";
   const existingPdfBytes = await fetch(url).then((res) => res.arrayBuffer());
 
   const pdfDoc = await PDFDocument.load(existingPdfBytes);
-  const pages = pdfDoc.getPages();
-  const firstPage = pages[0];
-  let tinggiAwal = 347;
-  let panjangAwal = 0;
-  let x = 265;
+  const form = pdfDoc.getForm();
 
-  const format = () => {
-    const { width, height } = firstPage.getSize();
-
-    return {
-      x: x + panjangAwal,
-      y: height - tinggiAwal,
-      size: 11,
-      color: rgb(0, 0, 0),
-    };
-  };
-
-  firstPage.drawText(name, format(), (tinggiAwal += 15));
-  firstPage.drawText(jk, format(), (tinggiAwal += 16));
-  //firstPage.drawText(bin, format(), (tinggiAwal += 22));
-  firstPage.drawText(lahir, format(), (tinggiAwal += 16));
-  firstPage.drawText(wargaNegara, format(), (tinggiAwal += 16));
-  //firstPage.drawText(agama, format(), (tinggiAwal += 22));
-  firstPage.drawText(nik, format(), (tinggiAwal += 16));
-  firstPage.drawText(pekerjaan, format(), (tinggiAwal += 16));
-  firstPage.drawText(alamat, format(), (tinggiAwal += 16));
-  firstPage.drawText(rt, {
-    x: 398,
-    y: 442,
-    size: 11,
-    color: rgb(0, 0, 0),
-  });
-  firstPage.drawText(tanggal, {
-    x: 455,
-    y: 442,
-    size: 11,
-    color: rgb(0, 0, 0),
-  });
-  firstPage.drawText(tanggal, {
-    x: 433,
-    y: 265,
-    size: 11,
-    color: rgb(0, 0, 0),
-  });
-  firstPage.drawText("Sodikun", {
-    x: 427,
-    y: 163,
-    size: 11,
-    color: rgb(0, 0, 0),
-  });
+  form.getTextField("nama").setText(name);
+  form.getTextField("jk").setText(jk);
+  form.getTextField("ttl").setText(lahir);
+  form.getTextField("warganegara").setText(wargaNegara);
+  form.getTextField("nik").setText(nik);
+  form.getTextField("pekerjaan").setText(pekerjaan);
+  form.getTextField("alamat").setText(alamat);
+  form.getTextField("rt").setText(rt);
+  form.getTextField("tanggal").setText(tanggal);
 
   const pdfBytes = await pdfDoc.save();
 
